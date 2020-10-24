4e0460d68bf673604601dd9ec64db6f82acf51a5
KarpovDenis74 <fotokarpov@yandex.ru>
2020-10-24 20:24:22 +0300

    Добавил сериализатор для возможности изменения пользователей без полей role и email

--- users_and_auth/serializer.py
+++ users_and_auth/serializer.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
 
-class User_Serializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('username', 'role', 'email', 'confirmation_code',
+                  'password', 'bio', 'first_name', 'last_name')
+        model = get_user_model()
+
+
+class UserEditSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('username',
                   'password', 'bio', 'first_name', 'last_name')
         model = get_user_model()
